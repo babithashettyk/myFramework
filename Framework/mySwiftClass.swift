a259df43b70373dd07de5b34db7c0638a1362f03
Babitha Shetty K <babitha.shetty@globaldelight.com>
2021-11-02 13:05:30 +0530

    Added

---
+++ Framework/mySwiftClass.swift
@@ -0,0 +1,13 @@
+//
+//  mySwiftClass.swift
+//  Framework
+//
+//  Created by Babitha Shetty K on 02/11/21.
+//
+
+import Foundation
+public class Test{
+    public class func featureOne(){
+    print("one")
+    }
+}
